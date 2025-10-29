1ec30c419a85200173d6137c549c2b9f63f0850c
LiuLime <liulime2222@gmail.com>
2025-10-29 17:42:37 +0900

    revise cmd by latest CA_RFdiffusion responsitory

--- motif_gen/02_diffusion/cmd.sh
+++ motif_gen/02_diffusion/cmd.sh
@@ -1,1 +1,12 @@
-python ../../software/ca_rf_diffusion/rf_diffusion/run_inference.py --config-name aa inference.json_args="inputs/tasks.json"
+export PYTHONPATH=/work/lyt/serine-hydrolase-design/software/ca_rf_diffusion
+python ../../software/ca_rf_diffusion/rf_diffusion/run_inference.py \
+--config-name RFdiffusion_CA_inference \
+inference.num_designs=1 \
+inference.ckpt_path="../../software/ca_rf_diffusion/checkpoints/ca_rfd_diffusion.pt" \
+inference.output_prefix="outputs_test/out" \
+inference.input_pdb="inputs/simple_theozyme.pdb" \
+inference.ligand="mu1" \
+inference.ij_visible="abc" \
+contigmap.contigs="['18,A1-3,31,B5-7,45']" \
+motif_only_2d=true \
+diffuser.r3.noise_scale=0.05
